7e8ac5ade961103a55b6a60dd4e620f4dc2f8d00
prithijitsouvik <110048103+prithijitsouvik@users.noreply.github.com>
2022-07-26 22:59:45 +0530

    Update list.php

--- application/views/list.php
+++ application/views/list.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>codeigniter Tutorial</title>
+        <title>codeigniter Tutorial souvik</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     </head>
     <body>
